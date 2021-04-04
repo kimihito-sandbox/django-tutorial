41da915b49895a052d0d60da11e52e9900ca3996
kimihito <tatsurotamashiro@gmail.com>
2021-04-04 22:16:51 +0900

    Tweak
    https://docs.djangoproject.com/ja/1.11/intro/tutorial03/#raising-a-404-error

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.http import HttpResponse
+from django.http.response import Http404
 from django.shortcuts import render
 
 from .models import Question
@@ -16,7 +17,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+  try:
+    question = Question.objects.get(pk=question_id)
+  except Question.DoesNotExist:
+    raise Http404("Question does not exisit")
+  return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
